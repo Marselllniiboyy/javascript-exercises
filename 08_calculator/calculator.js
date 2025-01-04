6822e4796ffbc7e794487b7e598c4b6659e26064
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2025-01-04 14:42:47 +0800

    08_calculator done🔥

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  return arr.reduce((accumulator, curentValue) => {
+    return accumulator + curentValue;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  return arr.reduce((accumulator, curentValue) => {
+    return accumulator * curentValue;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  // if (b === 0) return 1;
+  // let result = 1;
+  // for (i = 0; i < b; i++) {
+  //   result = result * a;
+  // }
+  // return result;
+
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0 || a === 1) return 1;
+  let result = 1;
+
+  for (let i = a; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
+  // recursion
+  // if (a === 0) return 1;
+  // return a * factorial(a - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +55,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
