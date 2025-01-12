9941924debb89012d3a21b26a79ba24f0da933fd
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2025-01-12 12:46:24 +0800

    Palindroms done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+  const stemp = sentence.split(" ").join().replaceAll(",", "");
+  let paliSentence = "";
+  let sign = "";
+  console.log(stemp);
 
+  for (let i = stemp.length - 1; i >= 0; i--) {
+    if (stemp.charAt(i) === "!" || stemp.charAt(i) === ".") {
+      sign += stemp.charAt(i);
+    } else {
+      paliSentence = paliSentence + stemp.charAt(i);
+    }
+  }
+
+  if (sign) {
+    paliSentence = paliSentence + sign;
+  }
+
+  if (paliSentence.toLowerCase() === stemp.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
