64dafd9aaa4576fce6232dbb33bbbda2c0bd1ca2
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2025-01-24 00:41:07 +0800

    Get the title done, very eazyy

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const data = books;
+  return data.map((item) => item["title"]);
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
